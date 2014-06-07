a0a9c5f50e229e3968fb97f43fe59dbb0dc1cc7b
Manuel Garcia <mgarciap@gmail.com>
2014-06-07 01:12:14 -0300

    modifications to run in Cloud Foundry

--- server.js
+++ server.js
@@ -2,21 +2,38 @@ var express = require('express');
 var app = express();
 var RedisStore = require('connect-redis')(express);
 
-// for showing IP address
-var os = require('os');
-var ipv4 = os.networkInterfaces().eth0[0].address;
-
 app.use(express.cookieParser());
+
+
+// This can be parsed easily with https://www.npmjs.org/package/cfenv
+// Doing it this way to go into details
+var vcap_services = JSON.parse(process.env.VCAP_SERVICES)
+var credentials = vcap_services.rediscloud[0].credentials
+var host = credentials.hostname
+var port = credentials.port
+var user = credentials.user
+var password = credentials.password
+var redis_url = 'redis://' + ':' + password + '@' + host + ':' + port
+
+
 app.use(express.session({
-    store: new RedisStore({ url: process.env.REDIS_URL+'42' }),
+//    store: new RedisStore({ url: redis_url + '/0' }),
+    store: new RedisStore({ url: redis_url}),
     secret: 'demo'
 }));
 
 // default route
 app.get('/', function(req, res) {
-      // write IP address and session.name
-      res.write('IP:' + ipv4 + '\n');
-      res.end('Name: ' + req.session.name + '\n');
+      // write session.name
+      res.write('Name: ' + req.session.name + '\n\n');
+
+      // ENV variables
+      // write IP address app instance container and session.name
+      res.write('Container Port:' + process.env.VCAP_APP_PORT +'\n');
+
+      res.write('redis_url: ' + redis_url + '\n\n');
+      res.write('VCAP_APPLICATION environment variables: ' + '\n' + process.env.VCAP_APPLICATION + '\n\n');
+      res.end('VCAP_SERVICES environment variables: ' + '\n' + process.env.VCAP_SERVICES)
 });
 
 app.get('/set/:name', function(req, res) {
@@ -31,4 +48,4 @@ app.get('/clear', function(req, res) {
       res.redirect('/');
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.VCAP_APP_PORT || 3000);
